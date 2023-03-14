758d4e4ef4ed5eadf9fcec7a86548fac7ea7b2ae
“jose <josearias018@hotmail.com>
2023-03-13 23:14:07 -0300

    filter - find

--- main.js
+++ main.js
@@ -1,52 +1,78 @@
-function Persona(nombre, edad, correo) {
-    this.nombre = nombre;
-    this.edad = edad;
-    this.correo = correo;
+const usuarios = [
+    {id: 1, nombre: "gael", edad: 23, correo: "gaelmyers_23@gmail.com"},
+    {id: 2, nombre: "ariana", edad: 30, correo: "arianeishon_30@gmail.com"},
+    {id: 3, nombre: "ibrahim", edad: 26, correo: "ibragamer_26@gmail.com"},
+];
+
+const registro = () => {
+    for (let i = 0; i < usuarios.length; i++) {
+        console.log(`ID: ${usuarios[i].id}`);
+        console.log(`Nombre: ${usuarios[i].nombre}`);
+        console.log(`Edad: ${usuarios[i].edad}`);
+        console.log(`Correo: ${usuarios[i].correo}`);
+        console.log("---------------------------------");
+        }
 };
-// este array sera el contenedor de las personax
-const personas = []
 
-//personas por defecto
-const persona1 = new Persona('Gael', 23, ['gaelmyers_23@gmail.com']);
-const persona2 = new Persona('Ariana', 30, ['arianeishon_30@gmail.com']);
-const persona3 = new Persona('Ibrahim', 26, ['ibragamer_26@gmail.com']);
-
-// agregado de personas por defecto al array
-personas.push(persona1);
-personas.push(persona2);
-personas.push(persona3);
-
-
-// función que recorre el registro de cada persona
-function registro() {
-    for (let i = 0; i < personas.length; i++) {
-        console.log(`Nombre: ${personas[i].nombre}`);
-        console.log(`Edad: ${personas[i].edad}`);
-        console.log('Correo:');
-        for (let c = 0; c < personas[i].correo.length; c++) {
-        console.log(`${personas[i].correo[c]}`);
-        }
-        console.log("-------------------------");
-        }
+const agregadoUsuario = () => {
+    let nombre = prompt("Ingrese su nombre");
+    let edad = prompt("Ingrese su edad");
+    let correo = prompt("Ingrese su correo");
+    let nuevoRegistro = {
+    id: usuarios.length + 1,
+    nombre: nombre,
+    edad: edad,
+    correo: correo,
     };
+    usuarios.push(nuevoRegistro);
+    alert(`${nombre} se agrego al registro.`);
+    console.log(usuarios);
+};
 
-function personaNueva() {
-    const nombre = prompt("Ingrese su nombre:");
-    const edad = parseInt(prompt("Ingrese su edad:"));
-    const correo = prompt("Ingrese su correo:").split(',');
+const metodoFilter = () => {
+    let entrada2 = prompt("Ingrese el nombre del usuario");
+    let filtrados = usuarios.filter((persona) => persona.nombre.includes(entrada2.toLowerCase()));
+    filtrados.forEach((persona) => {
+        console.log(filtrados)
+        let mensaje = `
+        Id: ${persona.id}
+        nombre: ${persona.nombre}
+        edad: ${persona.edad}
+        correo: ${persona.correo}
+        `;
+    alert(mensaje);
+    });
+};
 
-    const persona = new Persona(nombre, edad, correo);
-    personas.push(persona);
+const metodoFind = () => {
+    let entradaFind = prompt("Ingrese el id del producto");
+    if (entradaFind == "") {
+        alert("Ingrese el id del usuario")
+    } else {
+        let encontrado = usuarios.find(item => item.id == entradaFind);
+        console.log(encontrado)
+    }
+};
 
-    console.log(`${nombre} se agrego al registro.`);
-}
-
-// repetir agregarMasPersonas mientras se ingresa "s"
-let agregarMasPersonas = true;
-while (agregarMasPersonas) {
-personaNueva();
-const respuesta = prompt("Quiere registrar otra persona? (s/n)");
-agregarMasPersonas = (respuesta.toLowerCase() === "s");
-}
-
-registro();
+let entrada = prompt(" 1- Registrar\n 2- Ver Registro (consola)\n 3- Buscar Nombre\n 4- Buscar ID (consola)\n 5- Salir");
+while (entrada != "5") {
+    switch (entrada) {
+        case "1":
+            agregadoUsuario();
+            break;
+        case "2":
+            registro();
+            break;
+        case "3":
+            metodoFilter();
+            break;
+        case "4":
+            metodoFind();
+            break;
+        default:
+            alert("La opcion ingresada es invalida")
+            break;
+    }
+    entrada = prompt(" 1- Registrar\n 2- Ver Registro (consola)\n 3- Buscar Nombre\n 4- Buscar ID (consola)\n 5- Salir");
+    
+};
